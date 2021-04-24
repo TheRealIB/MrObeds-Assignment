da069fa01280a65c4287569c7c9dc465dbd18430
Ibrahim Musa <44068748+TheRealIB@users.noreply.github.com>
2021-04-24 20:25:56 +0100

    Create firstAssignment7.js

---
+++ firstAssignment7.js
@@ -0,0 +1,6 @@
+///Write a JavaScript function to Uncapitalize? the first character of a string
+function Uncapitalize(str1){
+  return str1.charAt(0).toLowerCase() + str1.slice(1);
+}
+ console.log(Uncapitalize('Mr obeds assignment'));
+ 
